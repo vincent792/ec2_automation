c4e542c2fc2c96378f08cb3e47bdcfee3919a78d
vincent792 <vincentwambu9@gmail.com>
2023-10-17 13:19:37 +0300

    w

--- i.py
+++ i.py
@@ -3,8 +3,34 @@ import boto3
 # Create an EC2 client
 ec2 = boto3.client('ec2')
 
-# Define your user data scripts for VM2 and VM3 (as previously mentioned)
+# Define your user data 
+vm2_user_data = """
+#!/bin/bash
+# Install Docker and pull NGINX and MongoDB containers
+# Run your NGINX and MongoDB containers here
 
+yum update -y
+yum install docker -y
+systemctl start docker
+docker run -p 8081:80 -d nginx
+docker run -p 8082:80 -d nginx
+docker run --name mongodb1 -d -p 27017:27017 mongo
+
+"""
+
+vm3_user_data = """
+#!/bin/bash
+# Install Docker and pull NGINX and MongoDB containers
+# Run your NGINX and MongoDB containers here
+
+yum update -y
+yum install docker -y
+systemctl start docker
+docker run -p 8081:80 -d nginx
+docker run -p 8082:80 -d nginx
+docker run --name mongodb2 -d -p 27017:27017 mongo
+
+"""
 # Launch VM2
 response_vm2 = ec2.run_instances(
     ImageId='ami-041feb57c611358bd',
